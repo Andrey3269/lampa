41ed6e798f3eb2e06e59268c9990b8e3bbabb10a
Andrey3269 <63809115+Andrey3269@users.noreply.github.com>
2026-09-24 17:20:45 +0300

    Add files via upload

--- domen.js
+++ domen.js
@@ -272,7 +272,7 @@
         var reloadBtn = document.createElement('div');
         reloadBtn.className = 'lampa-fab-btn lampa-fab-reload';
         reloadBtn.setAttribute('title', 'Перезагрузить страницу');
-        reloadBtn.innerHTML = '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"><path d="M3 12a9 9 0 0 1 15.5-6.36L21 8"></path><path d="M21 3v5h-5"></path><path d="M21 12a9 9 0 0 1-15.5 6.36L3 16"></path><path d="M3 21v-5h5"></path></svg>';
+        reloadBtn.innerHTML = '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z" fill="currentColor"></path></svg>';
         reloadBtn.addEventListener('click', function (e) {
             e.stopPropagation();
             window.location.reload();
